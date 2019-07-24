5ee010ba0d0305862ae69bd39e0316ad7e9969f1
Mason Balbera <mhbalbera@gmail.com>
2019-07-24 17:08:49 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -12,7 +12,7 @@ def line(katz_deli)
 end
 
 def take_a_number(katz_deli, name)
-  name ||= customer
+  name ||= "customer #{katz_deli.length + 1} "
   katz_deli << name
   puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
 end
